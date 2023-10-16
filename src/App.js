b79e5eb7dd6bfc2ae9cdd62beb41adf95437f918
Joaquin <joaquin.valderrama.e@gmail.com>
2023-10-16 13:59:34 -0500

    primeros cambios y barra de navegacion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import "bootstrap/dist/css/bootstrap.min.css"
+import "@fortawesome/react-fontawesome"
 export default function App() {
   return (
     <React.StrictMode>
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {Navbar, Container} from "react-bootstrap"
+import {Navbar, Container,Nav, NavDropdown} from "react-bootstrap"
 import logo from '../img/COLOR2.png'
 import './style/style.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons'
 export default function 
 () {
   return (
@@ -12,6 +13,33 @@ export default function
             <Navbar.Brand href="#">
             <img src={logo} width="200" alt="" height="30" className='d-inline-block align-top my-2'/>
             </Navbar.Brand>
+            <Navbar.Toggle aria-controls='menu'/>
+            <Navbar.Collapse id="menu">
+              <Nav className="ms-auto">
+                <Nav.Link href="#" className="nav_text">Inicio</Nav.Link>
+                <Nav.Link href="#" className="nav_text">Nosotros</Nav.Link>
+                <NavDropdown title="Productos" id="menudrop">
+                <NavDropdown title="Linea Agricola" id="menuagri">
+                  <NavDropdown.Item href="#">Micro Tunel</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Macro Tunel</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Mulch Negro</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Mulch Negro/Blanco</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Mangas de Regadío</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Sacos de Fertilizantes</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Bolsas para Viveros</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Agrofilm</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Linea Industrial" id="menuind">
+                  <NavDropdown.Item href="#">Lámina de Enferdado</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Bolsa para Empaques</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Láminas Termocontraibles</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Bolsas Termocontraibles</NavDropdown.Item>
+                </NavDropdown>
+                  
+                </NavDropdown>
+                <Nav.Link href="https://forms.gle/YSuKV6CUXB32S4jB9" target="_blank" className="nav_text">Agendar Reunión<FontAwesomeIcon icon={faArrowRight} className='ms-2'/></Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
         </Container>
         
     </Navbar>
